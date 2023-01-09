d9fab2bf84e8fcff1834390b614f27a15e47c064
Higashimoto Kanami <summerwave5959@gmail.com>
2023-01-09 12:21:23 +0900

    [Add] worksにsplide追加

--- js/style.js
+++ js/style.js
@@ -6,3 +6,32 @@ page_top.addEventListener("click", () => {
         behavior: "smooth"
     });
 });
+
+//Daily UIのスライド動作
+const options = {
+  mediaQuery: "min", 
+  fixedWidth: "24rem",
+  heightRatio: 0.3,
+  gap: 16,
+  type: "loop",
+  arrows: false,
+  drag: "free", 
+  flickPower: 100,
+  pagination: false,
+
+  autoScroll: {
+    speed: 0.5, 
+    pauseOnHover: false,
+    pauseOnFocus: true,
+  },
+  breakpoints: {
+    1025: {
+      gap: 24,
+      fixedWidth: "36rem",
+    },
+  },
+};
+
+const splide = new Splide(".splide", options);
+
+splide.mount(window.splide.Extensions);
